bcff05d02376b085b8929411e7dd11ffd7518c1b
Jerry <jerry@fly.idv.tw>
2014-09-10 20:31:26 +0800

    No Picture Handle

--- C311407_Mp4Platform/uploadVideo.php
+++ C311407_Mp4Platform/uploadVideo.php
@@ -1,67 +1,71 @@
 <?php header("Access-Control-Allow-Origin: *"); ?>
 <?php
 /*
- * POST參數
- * MUID
- * VideoID
- * videoFileName
- * */
+ * POST參數 MUID VideoID videoFileName
+ */
 include_once 'class_PostParamter.php';
 include_once 'class_AjaxMessage.php';
 include_once 'class_fileInfo.php';
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+ini_set ( 'display_errors', 1 );
+error_reporting ( E_ALL );
 
-define('rootPATH', __DIR__);
+define ( 'rootPATH', __DIR__ );
 
-$ajMessage = new ajaxMessage();
+$ajMessage = new ajaxMessage ();
 
-if ($_FILES["file"]["error"] > 0) {
-
+if ($_FILES ["file"] ["error"] > 0) {
+	
 	$ajMessage->result = false;
-	$ajMessage->message = $_FILES["file"]["error"];
-
+	$ajMessage->message = $_FILES ["file"] ["error"];
 } else {
-
-	$getFileName = $_FILES["file"]["name"];
-	$getFileType = $_FILES["file"]["type"];
-	$getFileSize = $_FILES["file"]["size"];
-	$getFileTmpPath = $_FILES["file"]["tmp_name"];
-
-	$splitFileName = explode(".", $getFileName);
-	$getExtension = end($splitFileName);
-
-	//回傳JSON取得檔案資訊
-	$ajMessage->fileInfo = new fileInfo();
-
+	
+	$getFileName = $_FILES ["file"] ["name"];
+	$getFileType = $_FILES ["file"] ["type"];
+	$getFileSize = $_FILES ["file"] ["size"];
+	$getFileTmpPath = $_FILES ["file"] ["tmp_name"];
+	
+	$splitFileName = explode ( ".", $getFileName );
+	$getExtension = end ( $splitFileName );
+	
+	// 回傳JSON取得檔案資訊
+	$ajMessage->fileInfo = new fileInfo ();
+	
 	$ajMessage->fileInfo->fileName = $getFileName;
 	$ajMessage->fileInfo->fileType = $getFileType;
 	$ajMessage->fileInfo->fileSize = $getFileSize;
 	$ajMessage->fileInfo->fileExtension = $getExtension;
-
-	//取得Post參數
+	
+	// 取得Post參數
 	$postInfo = new postParamater ();
 	
-	$postInfo->MUID = $_POST["MUID"];
-	$postInfo->VideoID = $_POST["VideoID"];
-	$postInfo->videoFileName = $_POST["videoFileName"];
-	$postInfo->videoAbsolutePath = $_POST["videoAbsolutePath"];
-	$postInfo->vodUrl = $_POST["vodUrl"];
+	$postInfo->MUID = $_POST ["MUID"];
+	$postInfo->VideoID = $_POST ["VideoID"];
+	$postInfo->videoFileName = $_POST ["videoFileName"];
+	$postInfo->videoAbsolutePath = $_POST ["videoAbsolutePath"];
+	$postInfo->vodUrl = $_POST ["vodUrl"];
+	$postInfo->photoAbsolutePath = $_POST ["photoAbsolutePath"];
+	$postInfo->photoFileName = $_POST ["photoFileName"];
 	
-	//$defFolder = "VOD";
-	$videoAbsolutePath = rootPATH."/".$postInfo->videoAbsolutePath;
-	$ajMessage->message = $videoAbsolutePath;
-	//檢查路徑
-	if(!file_exists($videoAbsolutePath)){
-		mkdir($videoAbsolutePath, 0777, true);
+	// $defFolder = "VOD";
+	$videoAbsolutePath = rootPATH . "/" . $postInfo->videoAbsolutePath;
+	//$ajMessage->message = $videoAbsolutePath;
+	// 檢查路徑
+	if (! file_exists ( $videoAbsolutePath )) {
+		mkdir ( $videoAbsolutePath, 0777, true );
 	}
-	
-	$isOK = move_uploaded_file($getFileTmpPath,$videoAbsolutePath."/".$postInfo->videoFileName);
-	$ajMessage->src = $postInfo->vodUrl."/".$postInfo->videoAbsolutePath."/".$postInfo->videoFileName;
-	$ajMessage->result = true;
+	try {
+		$isOK = move_uploaded_file ( $getFileTmpPath, $videoAbsolutePath . "/" . $postInfo->videoFileName );
+		$isOK = copy ( rootPATH . "/no_picture_available.jpg", rootPATH . "/" . $postInfo->photoAbsolutePath . "/" . $postInfo->photoFileName );
+		$ajMessage->src = $postInfo->vodUrl . "/" . $postInfo->videoAbsolutePath . "/" . $postInfo->videoFileName;
+		$ajMessage->result = true;
+		$ajMessage->message = rootPATH . "/" . $postInfo->photoAbsolutePath . "/" . $postInfo->photoFileName;
+	} catch ( Exception $e ) {
+		$ajMessage->message = $e->getMessage ();
+		$ajMessage->result = false;
+	}
 }
 
-$jsonString =  json_encode((array)$ajMessage);
-print_r($jsonString);
+$jsonString = json_encode ( ( array ) $ajMessage );
+print_r ( $jsonString );
 ?>
